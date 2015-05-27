9d38dd15de6af93cb623acd8874aa6d03a7d5090
Ryan Campbell <campbellr@gmail.com>
2015-05-26 20:25:06 -0600

    project skeleton

---
+++ README.rst
@@ -0,0 +1,27 @@
+===============
+smashrun-client
+===============
+
+A Python client for the Smashrun_ API.
+
+Install
+=======
+
+This package isn't on PyPi yet, so the easiest way to install is directly
+from the git repository::
+
+    $ pip install git+git://github.com/campbellr/smashrun-client.git
+
+Usage
+=====
+
+TODO
+
+Contributing
+============
+
+Contributions are greatly appreciated! Feel free to submit a pull request, or file
+an issue in our `issue tracker`_.
+
+.. _Smashrun: https://smashrun.com
+.. _issue tracker: https://github.com/campbellr/smashrun-client/issues
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    'requests',
+    'requests-oauthlib'
+]
+
+test_requirements = []
+
+setup(
+    name='smashrun-client',
+    version='0.1.0-dev',
+    description="A Python client for the Smashrun API",
+    long_description=readme + '\n\n' + history,
+    author="Ryan Campbell",
+    author_email='campbellr@gmail.com',
+    url='https://github.com/campbellr/smashrun-client',
+    packages=[
+        'smashrun',
+    ],
+    package_dir={'smashrun':
+                 'smashrun'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache",
+    zip_safe=False,
+    keywords='smashrun',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
