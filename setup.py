49300c6860ca3f335b6b5f773015dc8be9a4ac63
Ryan Campbell <campbellr@gmail.com>
2016-12-02 20:26:28 -0700

    stop explicitly python 2.6
    I can't get tox to run with py26 without running into
    ssl-related issues, so i'm going to stop testing 2.6.
    
    If it works, great. If it doesn't then i don't think anyone will
    actually notice.

--- setup.py
+++ setup.py
@@ -47,11 +47,11 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='unittest2.collector',
     tests_require=test_requirements
